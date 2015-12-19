f9d7ed2a9ff2dc3a1092beeea68005503a388033
Gino <ginowashere@hotmail.com>
2015-12-19 23:08:36 +1100

    added upwards movement to cube ~ Christian is poop.

---
+++ ProjectPoseidon/Assets/Scripts/Ham.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ham : MonoBehaviour {
+
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ProjectPoseidon/Assets/Scripts/TheBasics.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheBasics : MonoBehaviour {
+    //initialization of a private variable
+    private Ham _hamScript;
+    //initialization of a public variable
+    public GameObject cubeObject;
+    public float moveSpeed;
+    private Transform _cubeTransform;
+
+    // Use this for initialization
+    void Start () {
+        _cubeTransform = cubeObject.transform; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("space"))
+        {
+            _cubeTransform.position = new Vector3(0, _cubeTransform.position.y + (moveSpeed * Time.deltaTime), 0);
+        }
+	}
+}
