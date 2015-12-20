d61218b61aa4acf2a23957f7ae1a653c6ced224f
Christian McFarlane <1002183@student.sae.edu.au>
2015-12-21 00:09:59 +1100

    Did some more edits to the Controller scripts, added info into the editor so the button now looks like it should work. Fixed some other stuff in the controller, such as an enum which allows for the disabling of data sending on runtime. Added comments and made stuff hopefully easier to read and later edit if need be

--- ProjectPoseidon/Assets/Editor/UDPControllerEditor/UDPMultiControllerClientEditor.cs
+++ ProjectPoseidon/Assets/Editor/UDPControllerEditor/UDPMultiControllerClientEditor.cs
@@ -5,64 +5,124 @@ using UnityEditor;
 [CustomEditor(typeof(UDPMultiControllerClient))]
 public class UDPMultiControllerClientEditor : Editor
 {
-	GUIContent controlName = new GUIContent ("Control Name", "Name to match on the corresponding serverside object");
-	GUIContent methodToCall = new GUIContent ("Method to Call", "Name of the corresponding method on the server object");
-	GUIContent controlType = new GUIContent ("Controller Type", "Type of controller");
+    GUIContent UDPManagerContent = new GUIContent("UDP Manager", "Place here the empty gameobject which has the UDP manager script on it");
+    GUIContent controlNameContent = new GUIContent("Control Name", "Name to match on the corresponding serverside object");
+    GUIContent methodToCallContent = new GUIContent("Method to Call", "Name of the corresponding method on the server object");
+    GUIContent controlTypeContent = new GUIContent("Controller Type", "Type of controller");
+    GUIContent sendDataContent = new GUIContent("Enable Data Sending?", "Will send data to object depending on option");
+    GUIContent buttonTransTypeContent = new GUIContent("Transition", "Transition for the button to undergo when clicked");
 
-	public SerializedProperty 
-		//debug,
-		udpManager,
-		controllerType,
-		defaultButtonImg,
-		clickButtonImg;
+    public SerializedProperty
+    #region GenericProperties
+        udpManager,
+        sendDataEnum,
+        controllerType,
+    #endregion
+    #region ButtonProperties
+        buttonTransType,
+        defaultButtonImg,
+        clickButtonImg,
+        disableButtonImg,
+        normalColourImg,
+        clickedColourImg,
+        disabledColourImg,
+        fadeDurationImg;
+    #endregion
 
 
-	void OnEnable ()
-	{
-		// Setup the SerializedProperties
-		//debug = serializedObject.FindProperty ("_DeleteMe");
-		udpManager = serializedObject.FindProperty ("udpManager");
-		controllerType = serializedObject.FindProperty ("selectedControllerType");
-		defaultButtonImg = serializedObject.FindProperty ("DefaultButtonImage");
-		clickButtonImg = serializedObject.FindProperty ("ClickedButtonImage");
-	}
+    void OnEnable()
+    {
+        // Setup the SerializedProperties
+        #region GenericSetup
+        udpManager = serializedObject.FindProperty("udpManager");
+        sendDataEnum = serializedObject.FindProperty("selectedSendData");
+        controllerType = serializedObject.FindProperty("selectedControllerType");
+        #endregion
+        #region ButtonSetup
+        buttonTransType = serializedObject.FindProperty("selectedButtonTransition");
+        defaultButtonImg = serializedObject.FindProperty("defaultButtonImage");
+        clickButtonImg = serializedObject.FindProperty("clickedButtonImage");
+        disableButtonImg = serializedObject.FindProperty("disabledButtonImage");
+        normalColourImg = serializedObject.FindProperty("normalColour");
+        clickedColourImg = serializedObject.FindProperty("pressedColour");
+        disabledColourImg = serializedObject.FindProperty("disabledColour");
+        fadeDurationImg = serializedObject.FindProperty("fadeDuration");
+        #endregion
+    }
 
-	public override void OnInspectorGUI ()
-	{
-		serializedObject.Update ();
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
 
-		UDPMultiControllerClient udpControllerScript = (UDPMultiControllerClient)target;
-		EditorGUILayout.PropertyField (controllerType, controlType);
-		UDPMultiControllerClient.ControllerType conType = (UDPMultiControllerClient.ControllerType)controllerType.enumValueIndex;
-		udpControllerScript.selectedControllerType = (UDPMultiControllerClient.ControllerType)controllerType.enumValueIndex;
+        UDPMultiControllerClient udpControllerScript = (UDPMultiControllerClient)target;
 
-		EditorGUILayout.PropertyField (udpManager, controlName);
-		//EditorGUILayout.PropertyField (debug, controlName);
+        #region Enum Variables
+        EditorGUILayout.PropertyField(sendDataEnum, sendDataContent);
+        UDPMultiControllerClient.SendData sendType = (UDPMultiControllerClient.SendData)sendDataEnum.enumValueIndex;
+        udpControllerScript.selectedSendData = (UDPMultiControllerClient.SendData)sendDataEnum.enumValueIndex;
 
-		switch (conType) {
-		case UDPMultiControllerClient.ControllerType.Button:
-			DefaultControls ();
-			EditorGUILayout.PropertyField (defaultButtonImg);
-			EditorGUILayout.PropertyField (clickButtonImg);
-			break;
+        EditorGUILayout.PropertyField(controllerType, controlTypeContent);
+        UDPMultiControllerClient.ControllerType conType = (UDPMultiControllerClient.ControllerType)controllerType.enumValueIndex;
+        udpControllerScript.selectedControllerType = (UDPMultiControllerClient.ControllerType)controllerType.enumValueIndex;
+        #endregion
 
-		case UDPMultiControllerClient.ControllerType.DPad:
-			DefaultControls ();
-			break;
-		case UDPMultiControllerClient.ControllerType.Joystick:
-			DefaultControls ();
-			break;
-		case UDPMultiControllerClient.ControllerType.Gyro:
-			DefaultControls ();
-			break;
-		}
-		serializedObject.ApplyModifiedProperties ();
-	}
+        EditorGUILayout.PropertyField(udpManager, UDPManagerContent);
+        switch (conType)
+        {
+            #region ButtonGUIRegion
+            case UDPMultiControllerClient.ControllerType.Button:
+                EditorGUILayout.HelpBox("None of this is implemented Yet", MessageType.Error, true);
+                DefaultControls();
+                //
+                EditorGUILayout.PropertyField(buttonTransType, buttonTransTypeContent);
+                UDPMultiControllerClient.ButtonTransitionType TransType = (UDPMultiControllerClient.ButtonTransitionType)buttonTransType.enumValueIndex;
+                udpControllerScript.selectedButtonTransition = (UDPMultiControllerClient.ButtonTransitionType)buttonTransType.enumValueIndex;
+                //
+                switch (TransType)
+                {
+                    case UDPMultiControllerClient.ButtonTransitionType.None:
+                        EditorGUILayout.PropertyField(defaultButtonImg);
+                        break;
+                    case UDPMultiControllerClient.ButtonTransitionType.Sprite_Swap:
+                        EditorGUILayout.PropertyField(defaultButtonImg);
+                        EditorGUILayout.PropertyField(clickButtonImg);
+                        EditorGUILayout.PropertyField(disableButtonImg);
+                        break;
+                    case UDPMultiControllerClient.ButtonTransitionType.Colour_Tint:
+                        EditorGUILayout.PropertyField(defaultButtonImg);
+                        EditorGUILayout.PropertyField(normalColourImg);
+                        EditorGUILayout.PropertyField(clickedColourImg);
+                        EditorGUILayout.PropertyField(disabledColourImg);
+                        EditorGUILayout.PropertyField(fadeDurationImg);
+                        break;
+                }
+                break;
 
-	void DefaultControls ()
-	{
-		UDPMultiControllerClient udpControllerScript = (UDPMultiControllerClient)target;
-		udpControllerScript.controlName = EditorGUILayout.TextField (controlName, udpControllerScript.controlName);
-		udpControllerScript.methodToCall = EditorGUILayout.TextField (methodToCall, udpControllerScript.methodToCall);
-	}
+            #endregion
+
+            case UDPMultiControllerClient.ControllerType.DPad:
+                EditorGUILayout.HelpBox("None of this is implemented Yet", MessageType.Error, true);
+                DefaultControls();
+                break;
+            case UDPMultiControllerClient.ControllerType.Joystick:
+                EditorGUILayout.HelpBox("None of this is implemented Yet", MessageType.Error, true);
+                DefaultControls();
+                break;
+            case UDPMultiControllerClient.ControllerType.Gyro:
+                break;
+        }
+        switch (sendType)
+        {
+            default:
+                break;
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    void DefaultControls()
+    {
+        UDPMultiControllerClient udpControllerScript = (UDPMultiControllerClient)target;
+        udpControllerScript.controlName = EditorGUILayout.TextField(controlNameContent, udpControllerScript.controlName);
+        udpControllerScript.methodToCall = EditorGUILayout.TextField(methodToCallContent, udpControllerScript.methodToCall);
+    }
 }
--- ProjectPoseidon/Assets/Scripts/UDPScripts/UDP Controllers/UDPMultiControllerClient.cs
+++ ProjectPoseidon/Assets/Scripts/UDPScripts/UDP Controllers/UDPMultiControllerClient.cs
@@ -4,83 +4,131 @@ using System.Collections;
 
 public class UDPMultiControllerClient : MonoBehaviour
 {
-	//Declaration of Enum which holds what types of controls this script supports
-	public enum ControllerType
-	{
-		DPad,
-		Joystick,
-		Button,
-		Gyro
-	}
-	//Holds the currently selected controller in the editor
-	public ControllerType selectedControllerType;
+    //Declaration of Enum which holds what types of controls this script supports
+    public enum ControllerType
+    {
+        DPad,
+        Joystick,
+        Button,
+        Gyro
+    }
+    //Holds the currently selected controller in the editor
+    public ControllerType selectedControllerType;
 
-	//General Variables which will be sent to the server to aid in communication
-	//Holds the UDPManaged component so that messages can be sent to server.
-	public UDPManager udpManager;
-	//Holds the name of the control which will be accessed
-	public string controlName;
-	//Holds the name of the custom method to call on the object
-	public string methodToCall;
+    //Declaration of Enum which will define whether the script will be able to send data or not
+    public enum SendData
+    {
+        SendData,
+        DoNotSendData
+    }
+    //Holds the currently selected option for the sending of data
+    public SendData selectedSendData;
 
-	#region Button Type Variables
-	//Variables which are used in the button controller enum option
-	//Image to be displayed on button prior to click
-	public Sprite DefaultButtonImage;
-	//Image to be displayed on the button as it is being clicked.
-	public Sprite ClickedButtonImage;
+    //General Variables which will be sent to the server to aid in communication
+    //Holds the UDPManaged component so that messages can be sent to server.
+    public UDPManager udpManager;
+    //Holds the name of the control which will be accessed
+    public string controlName;
+    //Holds the name of the custom method to call on the object
+    public string methodToCall;
+
+    #region Button Type Variables
+    //Variables which are used in the button controller enum option
+    public enum ButtonTransitionType
+    {
+        None, Colour_Tint, Sprite_Swap
+    }
+    public ButtonTransitionType selectedButtonTransition;
+    //Default sprite to be displayed at all times
+    public Sprite defaultButtonImage;
+    #region ColourTintVariables
+    //Default colour of sprite
+    public Color normalColour;
+    //Pressed colour of sprite
+    public Color pressedColour;
+    //Disabled colour of sprite
+    public Color disabledColour;
+    //Duration for fade of click
+    public float fadeDuration;
+    #endregion
+    #region SpriteSwap
+    //Image to be displayed on the button as it is being clicked.
+    public Sprite clickedButtonImage;
+    //Image to be displayed on the button if it is disabled.
+    public Sprite disabledButtonImage;
+    #endregion
     #endregion
 
     #region Gyro Type Variables
     private Quaternion _lastLocation;
-	#endregion
+    #endregion
 
 
-	void Start ()
-	{
-		//Check for which control is being used.
-		switch (selectedControllerType) {
-		case ControllerType.Gyro: //If the controller is to send Gyroscope info
-			//Enable the Gyro.
-			Input.gyro.enabled = true;
-			break;
-		}
-	}
+    void Start()
+    {
+        //Check for which control is being used.
+        switch (selectedControllerType)
+        {
+            case ControllerType.Gyro: //If the controller is to send Gyroscope info
+                                      //Enable the Gyro.
+                Input.gyro.enabled = true;
+                break;
 
-	// Update is called once per frame
-	void Update ()
-	{
-		switch (selectedControllerType) {
-		case ControllerType.Gyro: //If the controller is to send Gyroscope info
-                //If the current gyroattitute is not equal to last location, send new location. Saves number of packets sent.
-                if (Input.gyro.attitude != _lastLocation)
+            case ControllerType.Button:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Switch statement to check whether data is allowed to me sent
+        switch (selectedSendData)
+        {
+            //If the user is able to send Data
+            case SendData.SendData:
+                switch (selectedControllerType)
                 {
-                    //Call the send mesage function, with all required components needed from gyroscope. and send it directly to the UDPManager
-                    udpManager.sendString(FormatMessage(selectedControllerType.ToString(), controlName, methodToCall, (Input.gyro.attitude * GetRotFix()).ToString()));
-                    _lastLocation = Input.gyro.attitude;
-                }
-			break;
-		}
-	}
-	//Formats the message for it to be sent to the server
-	string FormatMessage (string _ControlType, string _ControlName, string _MethodCall, params string[] _ControlData)
-	{
-		//Depending on how many parameters have been passed into the method, send to server the data and all variables
-		switch (_ControlData.Length) {
-		case 0:
-			return (_ControlType + "/!/" + _ControlName + "/!/" + _MethodCall);
-		case 1:
-			return (_ControlType + "/!/" + _ControlName + "/!/" + _MethodCall + "/!/" + _ControlData [0]);
-		case 2:
-			return (_ControlType + "/!/" + _ControlName + "/!/" + _MethodCall + "/!/" + _ControlData [0] + "/!/" + _ControlData [1]);
-		case 3:
-			return (_ControlType + "/!/" + _ControlName + "/!/" + _MethodCall + "/!/" + _ControlData [0] + "/!/" + _ControlData [1] + "/!/" + _ControlData [2]);
-		default:
-			return (_ControlType + "/!/" + _ControlName + "/!/" + _MethodCall);
-		}
-	}
+                    case ControllerType.Gyro: //If the controller is to send Gyroscope info
+                                              //If the current gyroattitute is not equal to last location, send new location. Saves number of packets sent.
+                        if (Input.gyro.attitude != _lastLocation)
+                        {
+                            //Call the send mesage function, with all required components needed from gyroscope. and send it directly to the UDPManager
+                            udpManager.sendString(FormatMessage(selectedControllerType.ToString(), controlName, methodToCall, (Input.gyro.attitude * GetRotFix()).ToString()));
+                            _lastLocation = Input.gyro.attitude;
+                        }
+                        break;
 
-    //Gets a rotation fix for the Gyro
+                    case ControllerType.Button:
+
+                        break;
+                }
+                break;
+            case SendData.DoNotSendData:
+                //Do Nothing EveryFrame. Allows for the script to stop sending data, without disabling the script.
+                break;
+        }
+    }
+    //Formats the message for it to be sent to the server
+    string FormatMessage(string _ControlType, string _ControlName, string _MethodCall, params string[] _ControlData)
+    {
+        //Depending on how many parameters have been passed into the method, send to server the data and all variables
+        switch (_ControlData.Length)
+        {
+            case 0:
+                return (_ControlType + "/!/" + _ControlName + "/!/" + _MethodCall);
+            case 1:
+                return (_ControlType + "/!/" + _ControlName + "/!/" + _MethodCall + "/!/" + _ControlData[0]);
+            case 2:
+                return (_ControlType + "/!/" + _ControlName + "/!/" + _MethodCall + "/!/" + _ControlData[0] + "/!/" + _ControlData[1]);
+            case 3:
+                return (_ControlType + "/!/" + _ControlName + "/!/" + _MethodCall + "/!/" + _ControlData[0] + "/!/" + _ControlData[1] + "/!/" + _ControlData[2]);
+            default:
+                return (_ControlType + "/!/" + _ControlName + "/!/" + _MethodCall);
+        }
+    }
+
+    //Gets a rotation fix for the Gyro. Resets all of the values so it is consistent between orientations
     private Quaternion GetRotFix()
     {
         if (Screen.orientation == ScreenOrientation.Portrait)
