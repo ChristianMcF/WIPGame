9e2643ff53de82370961cac18a8d0f7700731a74
Christian McFarlane <1002183@student.sae.edu.au>
2015-12-21 17:43:03 +1100

    Added some scenes and test scripts to work on another project.

---
+++ ProjectPoseidon/Assets/Scripts/SoundBasedGame/AudioEyes.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioEyes : MonoBehaviour
+{
+
+    private Transform _playerEyes;
+
+    // Use this for initialization
+    void Start()
+    {
+        _playerEyes = transform.FindChild("PlayerEyes");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit _raycasthit;
+        Physics.Raycast(_playerEyes.position, _playerEyes.transform.forward, out _raycasthit, 100f);
+        Debug.DrawRay(_playerEyes.position, _playerEyes.transform.forward * 100, Color.red);
+        Debug.Log(_raycasthit.collider.tag);
+    }
+}
---
+++ ProjectPoseidon/Assets/Scripts/SoundBasedGame/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private GameObject _playerEyes;
+    public float lookSpeed;
+    public float moveSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        _playerEyes = transform.FindChild("PlayerEyes").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region PlayerLook
+        _playerEyes.transform.localRotation *= Quaternion.Euler(-Input.GetAxis("Mouse Y") * lookSpeed, 0, 0);
+        gameObject.transform.localRotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        #endregion
+        #region PlayerMovement
+        //gameObject.GetComponent<Rigidbody>().AddRelativeForce((Vector3.forward * Input.GetAxis("Vertical")) * moveSpeed);
+        #endregion
+    }
+}
